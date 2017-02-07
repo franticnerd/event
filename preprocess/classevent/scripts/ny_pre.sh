0189f8a5ffbcc1c2be082ad75db1447f0941355f
chao <chao.uiuc@gmail.com>
2017-02-06 21:53:12 -0600

    add preprocessing for class event

--- preprocess/classevent/scripts/ny_pre.sh
+++ preprocess/classevent/scripts/ny_pre.sh
@@ -1,4 +1,4 @@
 para_file='./ny_pre.yaml'
-python '../gen_clean_entities.py' $para_file
-python '../clean_tweets_with_entities.py' $para_file
+# python '../gen_clean_entities.py' $para_file
+# python '../clean_tweets_with_entities.py' $para_file
 python '../link_noun_and_entities.py' $para_file
